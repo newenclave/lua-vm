a88d8f417204864ad2c71f5d7fa412b9e88041b4
newenclave <pikratacid@gmail.com>
2014-11-11 13:13:14 +0300

    lua

--- main.cpp
+++ main.cpp
@@ -15,170 +15,51 @@
 
 namespace lo = lua::objects;
 
-lo::base_sptr g_table = NULL;
 
-lo::base_sptr create( lua_State *L, int idx );
-
-lo::base_sptr create_table( lua_State *L, int index )
+int lcall_print( lua_State *L )
 {
-    lua_pushvalue(L, index);
-    lua_pushnil(L);
-
-    std::shared_ptr<lo::table> new_table( new lo::table );
-    while (lua_next(L, -2)) {
-
-        lua_pushvalue(L, -2);
-        std::shared_ptr<lo::pair> new_pair
-                ( new lo::pair( create( L, -1 ), create( L, -2 ) ) );
-        new_table->push_back( new_pair );
-        lua_pop(L, 2);
+    lua::state ls(L);
+    int n = ls.get_top( );
+    for( int i=1; i<=n; ++i ) {
+        lo::base_sptr bp( ls.get_object( i, 1 ) );
+        std::cout << bp->str( );
     }
-
-    lua_pop(L, 1);
-
-    return new_table;
+    ls.clean_stack( );
+    return 0;
 }
 
-lua::objects::base_sptr create_string( lua_State *L, int idx )
+int set_callback( lua_State *L )
 {
-    size_t length = 0;
-    const char *ptr = lua_tolstring( L, idx, &length );
-    return lo::base_sptr( new lo::string( ptr, length ) );
-}
+    lua::state ls(L);
 
-lua::objects::base_sptr create( lua_State *L, int idx )
-{
-    int t = lua_type( L, idx );
-    switch( t ) {
-    case LUA_TBOOLEAN:
-        return lo::base_sptr(
-                    new lo::boolean( lua_toboolean( L, idx ) ));
-    case LUA_TLIGHTUSERDATA:
-        return lo::base_sptr(
-                    new lo::light_userdata( lua_touserdata( L, idx ) ));
-    case LUA_TNUMBER:
-        return lo::base_sptr(
-                    new lo::number( lua_tonumber( L, idx ) ));
-    case LUA_TSTRING:
-        return create_string( L, idx );
-    case LUA_TFUNCTION:
-        return lo::base_sptr(
-                    new lo::function( lua_tocfunction( L, idx ) ));
-    case LUA_TTABLE:
-        return g_table = create_table( L, idx );
-//    case LUA_TUSERDATA:
-//        return "userdata";
-//    case LUA_TTHREAD:
-//        return "thread";
-    }
-    return lo::base_sptr(new lo::nil);
-}
+    const char *name = ls.get<const char *>( 1 );
 
-int l_print( lua_State *L )
-{
-    int n = lua_gettop( L );
-
-    lua::state tvm( L );
-
-    if( n < 0 ) {
-        n = ( (n * -1) + 1 );
+    int tt = ls.get_type( 2 );
+    if( tt == LUA_TFUNCTION ) {
+        ls.set_value( name, 2 );
     }
 
-    for ( int b = 1; b <= n; ++b ) {
-        std::cout << create( tvm.get_state( ), b )->str( );
-    }
-
-    std::cout << "\n";
-    tvm.pop( n );
+    ls.clean_stack( );
 
     return 0;
 }
 
-typedef std::map<int, void *> map_type;
+int main( int argc, const char **argv )
+{  try {
 
-int test_call( lua_State *L )
-{
-    lua::state v( L );
-    int data = v.get<int>( );
+    const char * path = argc > 1 ? argv[1] : "test.lua";
 
-    unsigned long mm = v.get_from_global<unsigned long>( "global_table", "map" );
-    if( mm ) {
-        map_type *m((map_type *)mm);
-        std::cout << "Parameters: " << (*m)[data] << "\n";
-    }
+    lua::state ls;
 
-    return 0;
-}
+    ls.register_call( "print", &lcall_print );
+    ls.register_call( "set_callback", &set_callback );
 
-int get( lua_State *L )
-{
-    lua::state v( L );
-    v.push( 2 );
-    return 1;
-}
-
-static size_t path_root( const char *path )
-{
-    size_t res = 0;
-    while( ( *path != '.' ) && ( *path != '\0' ) ) {
-        ++path;
-        ++res;
-    }
-    return res;
-}
-
-int main( ) try
-{
-    lua::state v;
-    v.register_call( "print", l_print );
-
-    v.openlib( "string" );
-    v.openlib( "table" );
-    v.openlib( "math" );
-    v.openlib( "os" );
-
-    v.set( "gtest.maxpart.x", 1 );
-
-    lo::table *t(lo::new_table( ));
-    t->add( lo::new_string( "1" ) )
-        ->add( lo::new_string( "2" ) )
-        ->add( lo::new_string( "3" ) )
-        ->add( lo::new_string( "neljä" ), lo::new_table( )
-            ->add( lo::new_string( "|" ) )
-            ->add( lo::new_string( "|" ) )
-            ->add( lo::new_string( "|" ) )
-            ->add( lo::new_string( "|" ) )
-            )
-      ;
-
-    v.set_object( "gtest.maxpart.y", t );
-
-    v.check_call_error(v.load_file( "test.lua" ));
-
-    int l = v.get_table( "t.backward" );
-
-    int i = 0;
-
-    std::cout << lua_rawlen( v.get_state( ), -1 ) << "!\n";
-
-    v.push( );
-    while( lua_next( v.get_state( ), -2 ) ) {
-
-        v.push_value( -2 );
-
-        std::cout << v.get<std::string>( -1 ) << "\n";
-        std::cout << v.get<std::string>( -2 ) << "\n";
-        v.pop( 2 );
-    }
-
-    v.pop( l );
-
-    l_print( v.get_state( ) );
+    ls.check_call_error(ls.load_file( path ));
 
     return 0;
 
 } catch( const std::exception &ex ) {
     std::cerr << "Error: " << ex.what( ) << "\n";
     return 1;
-}
+}}
 
--- test.lua
+++ test.lua
@@ -1,60 +1,11 @@
 
 --print( mysin( 90 ), '\n' )
 
---print( mysin( 'sdfsdf' ), '\n')
-
-function test( a )
-    print(a, 'test!\n')
+function test_call(  )
+    print( "Hello!\n" )
 end
 
-t = {
-     name =  "$$$$$$$$"
-    ,value = "1"
-    ,forward = {
-         zero = 0
-        ,one  = 1
-        ,two  = 2
-     }
-    ,backward = {
-          'zero'
-         ,'one'
-         ,'two'
-         ,'three'
-         ,'four'
-         ,'five'
-    }
-    ,tg = globt
-}
+set_callback( 'wptr.calls.test', test_call )
 
-function create_table( id )
-
-    local class = {
-         call = print
-        ,id   = id
-    }
-
-    class.get_id = function( c )
-        return c.id
-    end
-    class.__gc = function( c )
-        print(c.id, ' deleted')
-    end
-    return class
-end
-
-a = create_table( 1 )
-b = create_table( 2 )
-
-function test( )
-    local c = create_table( 4 )
-end
-
---a:call( t )
-
-m = { ['t.tt'] = { i={ } } }
-
-print( math )
-print( string )
-print( gtest.maxpart.y )
-
+wptr.calls.test( )
 
